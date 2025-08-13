898f1cc31eadf638b9c5da6ca0001317d8044853
ChewyMochi <austinchen237@gmail.com>
2025-08-13 21:58:55 +0800

    Created Main.java for enumeration exercise test cases.

---
+++ Week 1/Exercises/Enum Exercise/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void describe(String colour, Priority priority) {
+        switch (priority) {
+            case HIGH:
+                System.out.println(colour + " indicates high priority");
+                break;
+            case MEDIUM:
+                System.out.println(colour + " indicates medium priority");
+                break;
+            case LOW:
+                System.out.println(colour + " indicates low priority");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        describe("Red", Priority.HIGH);
+        describe("Orange", Priority.MEDIUM);
+        describe("Blue", Priority.MEDIUM);
+        describe("Green", Priority.LOW);
+    }
+}
