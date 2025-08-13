f41bfb29df3b9dad73269d97feaf80584b4d2187
ChewyMochi <austinchen237@gmail.com>
2025-08-13 18:56:12 +0800

    Added Getters/Setters to Circle.java class.

--- Week 1/Exercises/Circle.java
+++ Week 1/Exercises/Circle.java
@@ -13,6 +13,30 @@ public class Circle {
         this.radius = radius;
     }
 
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
     public int getArea() {
         int area = (int)(Math.PI * Math.pow(this.radius, 2));
         return area;
